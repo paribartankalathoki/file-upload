5d5ef618f3f691141746113ac8b82d6cb87defa4
Paribartan Kalathoki <paribartankalathoki@gmail.com>
2022-03-25 00:19:45 +0545

    added csv file upload section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ImageUploadComponent } from './image-upload/image-upload.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CsvUploadComponent } from './csv-upload/csv-upload.component';
 @NgModule({
-  declarations: [AppComponent, ImageUploadComponent],
+  declarations: [AppComponent, ImageUploadComponent, CsvUploadComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/csv-upload/csv-upload.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-csv-upload',
+  templateUrl: './csv-upload.component.html',
+  styleUrls: ['./csv-upload.component.scss'],
+})
+export class CsvUploadComponent implements OnInit {
+  fileName: string = '';
+  file: File | any;
+
+  @ViewChild('fileUpload') fileUpload: any;
+
+  message: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onFileUploadClick() {
+    this.fileUpload.nativeElement.value = '';
+  }
+
+  changeListener(files: any) {
+    console.log(files);
+    files = files.target.files;
+    if (files && files.length > 0) {
+      this.file = files.item(0);
+      console.log(this.file.name);
+    }
+  }
+
+  onUpload() {
+    console.log(this.file?.name);
+    console.log(this.file?.size);
+    console.log(this.file?.type);
+    if (!this.file) {
+      this.message = "Please select file";
+      return;
+    }
+    let reader: FileReader = new FileReader();
+    reader.readAsText(this.file);
+    reader.onload = (e) => {
+      let csv: string = reader.result as string;
+      // now you can save your data into database
+      console.log(csv);
+    };
+  }
+}
